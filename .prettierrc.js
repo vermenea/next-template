50b4191cdd8a5a18d8e285726ef6e49a863ede09
vermenea <vermenea@gmail.com>
2024-05-30 14:03:05 +0200

    next.js/tailwindCSS boilerplate

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+};
